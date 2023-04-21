a5f93d40c55ae60d7aa466ecf277a7b66d761a6a
Inna Shkilna <shkinai.1995@gmail.com>
2023-04-21 13:16:45 +0300

    performance of task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const elIngredients = document.querySelector('#ingredients');
+console.log(elIngredients);
+
+const liEl = ingredients.map(el => {
+  const elItem = document.createElement('li');
+
+  elItem.textContent = el;
+  elItem.classList = 'item';
+
+  return elItem;
+});
+
+elIngredients.append(...liEl);
