246bb9abb3537d2c0a74ee94c8a04978790bb63c
Inna Shkilna <shkinai.1995@gmail.com>
2023-04-22 14:55:28 +0300

    task performance 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+
+const textColor = document.querySelector('.color');
+
+function onButtonClick() {
+  const newColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newColor;
+
+  textColor.textContent = newColor;
+}
+
+btn.addEventListener('click', onButtonClick);
