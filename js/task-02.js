aaefc6d6927367274e314d6241779e2d963d4396
Inna Shkilna <shkinai.1995@gmail.com>
2023-04-22 14:57:00 +0300

    edit task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,7 +1,6 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
 const elIngredients = document.querySelector('#ingredients');
-console.log(elIngredients);
 
 const liEl = ingredients.map(el => {
   const elItem = document.createElement('li');
