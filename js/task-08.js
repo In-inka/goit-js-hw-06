ff21ed7d9a7a5e1cc0a719968fa5b17bdd25ecd5
Inna Shkilna <shkinai.1995@gmail.com>
2023-04-22 14:42:16 +0300

    task performance 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formLogin = document.querySelector('.login-form');
 
+formLogin.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = formLogin.email.value;
+  const password = formLogin.password.value;
+
+  if (!email || !password) {
+    alert('All fields must be filled!');
+  } else {
+    const valueForm = {
+      email: email,
+      password: password,
+    };
+    console.log(valueForm);
+  }
+  formLogin.reset();
+}
